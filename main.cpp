3397311532db370fa63e20ed87a0152f9d2aac68
Holger Klein <holgerhugoklein@gmail.com>
2017-07-22 20:32:42 +0200

    before revert!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 #include "CooTensor.h"
 
 int main() {
-    
-
+    CooTensor<6> tensor{};
+    for(size_t i = 0; i < 1000; ++i) {
+        uint32_t coords[6];
+        for(size_t j = 0; j < 6; ++j) {
+            coords[j] = 0 + (rand() % static_cast<int>(20 - 0 + 1));
+        }
+        numericVal val = 2;
+        tensor.add(coords, val);
+    }
 }
